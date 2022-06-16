48383228b00378ecaf8bd96621496265457f8496
icondev22 <icondev22@protonmail.com>
2022-06-16 12:23:26 +0545

    added new arrayUtils method

--- game-authorization-score/src/main/java/com/iconbet/score/authorization/utils/ArrayDBUtils.java
+++ game-authorization-score/src/main/java/com/iconbet/score/authorization/utils/ArrayDBUtils.java
@@ -59,4 +59,30 @@ public final class ArrayDBUtils {
         }
         return contains;
     }
+
+    public static <T> boolean containsInList(T value, List<T> listOfValues){
+        boolean contains = false;
+        if (listOfValues == null || value ==  null){
+            return contains;
+        }
+
+        for (int i = 0; i < listOfValues.size(); i++){
+            if (listOfValues.get(i) != null && listOfValues.get(i).equals(value)){
+                contains = true;
+                break;
+            }
+        }
+        return contains;
+    }
+
+    public static <T> List<T> arrayDBToList(ArrayDB<T> array){
+        List<T> listOfValues = new ArrayList<>();
+        if (array.size() == 0){
+            return listOfValues;
+        }
+        for (int i = 0; i < array.size(); i++){
+            listOfValues.add(array.get(i));
+        }
+        return listOfValues;
+    }
 }
