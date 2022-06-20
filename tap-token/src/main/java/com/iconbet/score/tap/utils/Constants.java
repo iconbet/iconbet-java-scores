18a533e79ea14e97ea6af24c1cac1953f9d66ebd
icondev22 <icondev22@protonmail.com>
2022-06-20 14:56:07 +0545

    added constants on constants file

---
+++ tap-token/src/main/java/com/iconbet/score/tap/utils/Constants.java
@@ -0,0 +1,57 @@
+package com.iconbet.score.tap.utils;
+
+import java.math.BigInteger;
+
+public class Constants {
+    public static final BigInteger DAY_TO_MICROSECOND = new BigInteger("86400000000");
+    public static final String BALANCES = "balances";
+    public static final String TOTAL_SUPPLY = "total_supply";
+    public static final String DECIMALS = "decimals";
+    public static final String ADDRESSES = "addresses";
+    public static final String IDS = "ids";
+    public static final String AMOUNT = "amount";
+    public static final String EVEN_DAY_CHANGES = "even_day_changes";
+    public static final String ODD_DAY_CHANGES = "odd_day_changes";
+
+    public static final String MAX_LOOPS = "max_loops";
+    public static final String INDEX_ADDRESS_CHANGES = "index_address_changes";
+    public static final String INDEX_UPDATE_BALANCE = "index_update_balance";
+    public static final String BALANCE_UPDATE_DB = "balance_update_db";
+    public static final String ADDRESS_UPDATE_DB = "address_update_db";
+
+    public static final String DIVIDENDS_SCORE = "dividends_score";
+    public static final String BLACKLIST_ADDRESS = "blacklist_address";
+
+    public static final String STAKED_BALANCES = "staked_balances";
+    public static final String MINIMUM_STAKE = "minimum_stake";
+    public static final String UNSTAKING_PERIOD = "unstaking_period";
+    public static final String TOTAL_STAKED_BALANCE = "total_staked_balance";
+
+    public static final String EVEN_DAY_STAKE_CHANGES = "even_day_stake_changes";
+    public static final String ODD_DAY_STAKE_CHANGES = "odd_day_stake_changes";
+
+    public static final String INDEX_STAKE_ADDRESS_CHANGES = "index_address_stake_changes";
+    public static final String INDEX_UPDATE_STAKE = "index_update_stake";
+    public static final String STAKE_UPDATE_DB = "stake_update_db";
+    public static final String STAKE_ADDRESS_UPDATE_DB = "stake_address_update_db";
+
+    public static final String STAKING_ENABLED = "staking_enabled";
+    public static final String SWITCH_DIVS_TO_STAKED_TAP_ENABLED = "switch_divs_to_staked_tap";
+
+    public static final String PAUSED = "paused";
+    public static final String PAUSE_WHITELIST = "pause_whitelist";
+    public static final String LOCKLIST = "locklist";
+
+//    public verify the usage of BigInteger
+
+    public static final String _LINEAR_COMPLEXITY_MIGRATION = "linear_complexity_migration";
+
+    public static final String _TIME_OFFSET = "time_offset";
+    public static final String _STAKE_SNAPSHOTS = "stake_snapshots";
+    public static final String _TOTAL_SNAPSHOTS = "total_snapshots";
+    public static final String _TOTAL_STAKED_SNAPSHOT = "total_staked_snapshot";
+    public static final String _TOTAL_STAKED_SNAPSHOT_COUNT = "total_staked_snapshot_count";
+
+    public static final String _ENABLE_SNAPSHOTS = "enable_snapshots";
+
+}
