03c048809b441ae92dbe405cc1f79f8e959f9365
icondev22 <icondev22@protonmail.com>
2022-06-20 14:53:45 +0545

    added constants file for constants

---
+++ player-nft-profile/src/main/java/com/iconbet/score/ibpnp/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.iconbet.score.ibpnp.utils;
+
+import java.util.List;
+
+public class Constants {
+    public static String ICONex = "ICONex";
+    public static String Ledger = "Ledger";
+    public static List<String> walletType = List.of(ICONex, Ledger);
+}
