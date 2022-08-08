97ea260da30d5c725c42921861af8af2a53382f3
icondev22 <icondev22@protonmail.com>
2022-08-08 10:07:48 +0545

    changed the storing varable type from Object to BigInteger

--- tap-token/src/main/java/com/iconbet/score/tap/Snapshot.java
+++ tap-token/src/main/java/com/iconbet/score/tap/Snapshot.java
@@ -11,12 +11,12 @@ public class Snapshot {
     public final VarDB<BigInteger> _time_offset = Context.newVarDB(_TIME_OFFSET, BigInteger.class);
 
     //        [address][snapshot_id]["ids" || "amount"]
-    public final BranchDB<Address, BranchDB<Integer, DictDB<String, Object>>> _stake_snapshots = Context.newBranchDB(_STAKE_SNAPSHOTS, Integer.class);
+    public final BranchDB<Address, BranchDB<Integer, DictDB<String, BigInteger>>> _stake_snapshots = Context.newBranchDB(_STAKE_SNAPSHOTS, BigInteger.class);
     //        [address] = total_number_of_snapshots_taken
     public final DictDB<Address, Integer> _total_snapshots = Context.newDictDB(_TOTAL_SNAPSHOTS, Integer.class);
 
     //        [snapshot_id]["ids" || "amount"]
-    public final BranchDB<Integer, DictDB<String, Object>> _total_staked_snapshot = Context.newBranchDB(_TOTAL_STAKED_SNAPSHOT, Integer.class);
+    public final BranchDB<Integer, DictDB<String, BigInteger>> _total_staked_snapshot = Context.newBranchDB(_TOTAL_STAKED_SNAPSHOT, BigInteger.class);
     public final VarDB<Integer> _total_staked_snapshot_count = Context.newVarDB(_TOTAL_STAKED_SNAPSHOT_COUNT, Integer.class);
 
     public final VarDB<Boolean> _enable_snapshots = Context.newVarDB(_ENABLE_SNAPSHOTS, Boolean.class);
